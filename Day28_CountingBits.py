15326a8a60bb1c01f69abf3606a168eed2ae1695
P-Swati <43266293+P-Swati@users.noreply.github.com>
2020-05-28 19:30:40 +0530

    Update Day28_CountingBits.py

--- Day28_CountingBits.py
+++ Day28_CountingBits.py
@@ -1,13 +1,18 @@
 class Solution:
     def countBits(self, num: int) -> List[int]:
-        #approach: the numbers n and (n&(n-1)), differ by only the least significant bit position
+        #approach 1: the numbers n and (n&(n-1)), differ by only the least significant bit position
         # eg. 8 -> 1000
         # and 8&7 -> 0000
         #hence we can say : number_of_1's_in_8 = 1 + num_of_1's_in_(8&7)
         
+        #approach 2: if we have the number of 1's for the number right shifted by 1 bit, then
+        # number_of_ones_in_num = number_of_ones_in_num>>1 + last_bit_of_num (0 or 1)
+        # eg. 9== 1001 => ans[1001] = ans[100] + 1 =2
+        
         ans=[0]*(num+1)
         
         for i in range(1,num+1):
-            ans[i]=1+ans[i&(i-1)]
-            
+            # ans[i]=1+ans[i&(i-1)] (1)
+            ans[i]=ans[i>>1]+(i&1) #(2)
+ 
         return ans
